c6f410f3f5cb97e3572c0e4e80e819a69ab01991
Theryston <funktodo2@gmail.com>
2021-03-10 19:25:30 -0300

    webscraping and server

--- server.py
+++ server.py
@@ -1,3 +1,14 @@
-from webscraping import get_word
+from webscraping import get
+from flask import Flask, request, jsonify
 
-print(get_word('amor'))
+app = Flask('dictionary API')
+
+@app.route('/', methods=["GET"])
+def index():
+	word = request.args.get('word')
+	if word:
+		return jsonify(get(word))
+	else:
+		return jsonify({"error": "palavra não inserida"})
+
+app.run(port=3131, debug=False)
--- webscraping.py
+++ webscraping.py
@@ -1,13 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_word(word):
-	url = 'https://delp.pt/'+word
+def get(word_input):
+	url = 'https://delp.pt/'+word_input
 	res = requests.get(url)
-	content = res.content
 	
-	site = BeautifulSoup(content, 'html.parser')
+	site = BeautifulSoup(res.content, 'html.parser')
 	
-	word_res = site.find('span', attrs={'class', 'varpb'}).text
+	word_shapes_raw = site.findAll('span', attrs={'class', 'varpb'})
+	word_shapes = []
+	for i, word in enumerate(word_shapes_raw):
+		if i < 2:
+			word_shapes.append(word.text)
+		
 	
-	return word_res
+	defs_list = site.findAll('span', attrs={'class', 'def'})
+	defs = []
+	for i, def_list in enumerate(defs_list):
+		if i == 0:
+			word_from = def_list.text
+		else:
+			defs.append(def_list.text)
+		
+		
+	return_datas = {
+		'word': word_shapes,
+		'defs': defs,
+		'latin': word_from
+	}
+	
+	return return_datas
